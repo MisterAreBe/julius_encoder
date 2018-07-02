75e8774f4bf8f8a481f34af4f5a9bda46fe7b7d0
Ricky Collins <richardcollins52317@gmail.com>
2018-07-02 15:46:34 -0400

    pass, test for uppercase letters to be encoded as well.

--- encoder.rb
+++ encoder.rb
@@ -1,10 +1,16 @@
 def encoder(message)
-    aplha_encoder = {"a" => "f", "b" => "g", "c" => "h", "d" => "i", "e" => "j", "f" => "k", "g" => "l", "h" => "m", "i" => "n", "j" => "o", "k" => "p", "l" => "q", "m" => "r", "n" => "s", "o" => "t", "p" => "u", "q" => "v", "r" => "w", "s" => "x", "t" => "y", "u" => "z", "v" => "a", "w" => "b", "x" => "c", "y" => "d", "z" => "e"}
+    alpha_encoder = {"a" => "f", "b" => "g", "c" => "h", "d" => "i", "e" => "j", "f" => "k", "g" => "l", "h" => "m", "i" => "n", "j" => "o", "k" => "p", "l" => "q", "m" => "r", "n" => "s", "o" => "t", "p" => "u", "q" => "v", "r" => "w", "s" => "x", "t" => "y", "u" => "z", "v" => "a", "w" => "b", "x" => "c", "y" => "d", "z" => "e"}
     mess_arr = message.split("")
     holder = ""
     mess_arr.each_with_index do |x, index|
-        if aplha_encoder.has_key?(x)
-            holder = aplha_encoder.values_at(x)
+        if alpha_encoder.has_key?(x)
+            holder = alpha_encoder.values_at(x)
+            mess_arr[index] = holder
+        elsif alpha_encoder.has_key?(x.downcase)
+            holder = alpha_encoder.values_at(x.downcase)
+            holder = holder.join("")
+            holder = holder.upcase
+            holder = holder.split("")
             mess_arr[index] = holder
         end
     end
