5f1b07b1d786c186b372ead997f22ffcad2ab766
Ricky Collins <richardcollins52317@gmail.com>
2018-07-02 16:09:19 -0400

    pass, test for message to get decoded

--- encoder.rb
+++ encoder.rb
@@ -18,6 +18,22 @@ def encoder(message)
     secret
 end
 
-def decoder(encoded_message)
-    ""
+def decoder(message)
+    message_decoder = alpha_encoder = {"a" => "f", "b" => "g", "c" => "h", "d" => "i", "e" => "j", "f" => "k", "g" => "l", "h" => "m", "i" => "n", "j" => "o", "k" => "p", "l" => "q", "m" => "r", "n" => "s", "o" => "t", "p" => "u", "q" => "v", "r" => "w", "s" => "x", "t" => "y", "u" => "z", "v" => "a", "w" => "b", "x" => "c", "y" => "d", "z" => "e"}.invert
+    mess_arr = message.split("")
+    holder = ""
+    mess_arr.each_with_index do |x, index|
+        if alpha_encoder.has_key?(x)
+            holder = alpha_encoder.values_at(x)
+            mess_arr[index] = holder
+        elsif alpha_encoder.has_key?(x.downcase)
+            holder = alpha_encoder.values_at(x.downcase)
+            holder = holder.join("")
+            holder = holder.upcase
+            holder = holder.split("")
+            mess_arr[index] = holder
+        end
+    end
+    revealed = mess_arr.join("")
+    revealed
 end
